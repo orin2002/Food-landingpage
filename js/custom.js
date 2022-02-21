48b20beac645d569aff095aa13600ec3fd470893
orin2002 <www.orinfahm@gmail.com>
2022-02-21 13:41:15 +0600

    final changes

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,13 @@
 $(document).ready(()=>{
+  // ================for sticky nav ==============//
     $(window).scroll(function(){
       var smoothScroll = $(this).scrollTop();
-      if(smoothScroll>50){
+      if(smoothScroll>100){
         $(".navbar").addClass("smooth-scroll");
       }else{
           $(".navbar").removeClass("smooth-scroll")
       }
-    })
+    });
+    // =================init wow js=======================//
+    new WOW().init();
 })
